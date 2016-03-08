b2792c0840611e8fd065b0fb381cdae9734eb9e4
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-03-08 14:00:54 -0500

    Hello, World! note

--- your-first-NSLog/FISAppDelegate.m
+++ your-first-NSLog/FISAppDelegate.m
@@ -7,11 +7,11 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     /**
-    
+NSLog(@"Hello, World!");
      * Hey! Write your code here! :D
      
      */
-    
+
     // do not alter
     return YES;  //
 }   ///////////////
